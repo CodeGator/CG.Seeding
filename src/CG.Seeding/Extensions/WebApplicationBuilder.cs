aa5e8d0cb9234cc6c4a2fef73c98ff6e4bc90add
Martin Cook <33993749+martycook@users.noreply.github.com>
2023-02-03 14:29:51 -0600

    I removed the check for development environment

--- src/CG.Seeding/Extensions/WebApplicationBuilder.cs
+++ src/CG.Seeding/Extensions/WebApplicationBuilder.cs
@@ -32,87 +32,75 @@ public static class WebApplicationExtensions
             "Checking the application's environment for the seeder."
             );
 
-        // We only touch the database in a development environment.
-        if (webApplication.Environment.IsDevelopment())
-        {
-            // Log what we are about to do.
-            webApplication.Logger.LogDebug(
-                "Fetching the {op} options, for the seeder.",
-                nameof(SeedingOptions)
-                );
+        // Log what we are about to do.
+        webApplication.Logger.LogDebug(
+            "Fetching the {op} options, for the seeder.",
+            nameof(SeedingOptions)
+            );
 
-            // Get the seeding options.
-            var seedingOptions = webApplication.Services.GetRequiredService<
-                IOptions<SeedingOptions>
-                >();
+        // Get the seeding options.
+        var seedingOptions = webApplication.Services.GetRequiredService<
+            IOptions<SeedingOptions>
+            >();
 
-            // Should we skip seeding altogether?
-            if (!seedingOptions.Value.SeedOnStartup)
-            {
-                // Log what we didn't do.
-                webApplication.Logger.LogWarning(
-                    "Ignoring seeding startup because the SeedOnStartup flag, " +
-                    "in the {op} options, is either false, or missing. This " +
-                    "can also happen if the {name}' method hasn't been called " +
-                    "from the application's startup",
-                    nameof(SeedingOptions),
-                    nameof(WebApplicationBuilderExtensions.AddSeeding)
-                    );
-
-                // Return the application.
-                return webApplication;
-            }
-
-            // Sanity check the file names list.
-            if (seedingOptions.Value.FileNames is null)
-            {
-                // Supply an empty list.
-                seedingOptions.Value.FileNames = new();
-            }
-
-            // Log what we are about to do.
-            webApplication.Logger.LogInformation(
-                "Seeding from {count} JSON files, for the seeder.",
-                seedingOptions.Value.FileNames.Count
-                );
-
-            // Log what we are about to do.
-            webApplication.Logger.LogDebug(
-                "Creating a DI scope, for the seeder."
-                );
-
-            // Create a DI scope.
-            using var scope = webApplication.Services.CreateScope();
-
-            // Log what we are about to do.
-            webApplication.Logger.LogDebug(
-                "Creating a seeding director instance, for the seeder."
-                );
-
-            // Get the seed director.
-            var director = scope.ServiceProvider.GetRequiredService<ISeedDirectorBase>();
-
-            // Log what we are about to do.
-            webApplication.Logger.LogTrace(
-                "Deferring to the {name} method, for the seeder",
-                nameof(ISeedDirectorBase.SeedAsync)
-                );
-
-            // Perform the seeding operation(s).
-            director.SeedAsync(
-                seedingOptions.Value.FileNames,
-                "seed",
-                seedingOptions.Value.Force
-                ).Wait();
-        }
-        else
+        // Should we skip seeding altogether?
+        if (!seedingOptions.Value.SeedOnStartup)
         {
             // Log what we didn't do.
             webApplication.Logger.LogWarning(
-                "Ignoring seeding startup because we aren't in a development " +
-                "environment."
+                "Ignoring seeding startup because the SeedOnStartup flag, " +
+                "in the {op} options, is either false, or missing. This " +
+                "can also happen if the {name}' method hasn't been called " +
+                "from the application's startup",
+                nameof(SeedingOptions),
+                nameof(WebApplicationBuilderExtensions.AddSeeding)
                 );
+
+            // Return the application.
+            return webApplication;
         }
+
+        // Sanity check the file names list.
+        if (seedingOptions.Value.FileNames is null)
+        {
+            // Supply an empty list.
+            seedingOptions.Value.FileNames = new();
+        }
+
+        // Log what we are about to do.
+        webApplication.Logger.LogInformation(
+            "Seeding from {count} JSON files, for the seeder.",
+            seedingOptions.Value.FileNames.Count
+            );
+
+        // Log what we are about to do.
+        webApplication.Logger.LogDebug(
+            "Creating a DI scope, for the seeder."
+            );
+
+        // Create a DI scope.
+        using var scope = webApplication.Services.CreateScope();
+
+        // Log what we are about to do.
+        webApplication.Logger.LogDebug(
+            "Creating a seeding director instance, for the seeder."
+            );
+
+        // Get the seed director.
+        var director = scope.ServiceProvider.GetRequiredService<ISeedDirectorBase>();
+
+        // Log what we are about to do.
+        webApplication.Logger.LogTrace(
+            "Deferring to the {name} method, for the seeder",
+            nameof(ISeedDirectorBase.SeedAsync)
+            );
+
+        // Perform the seeding operation(s).
+        director.SeedAsync(
+            seedingOptions.Value.FileNames,
+            "seed",
+            seedingOptions.Value.Force
+            ).Wait();
 
         // Return the application.
         return webApplication;
